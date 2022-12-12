fbd8471f6bf4880b8dac58c9b23695519352862e
Hector Ruiz <hectorruizlaph@gmail.com>
2022-12-12 12:34:03 -0700

    created env backend2

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,5 @@
-const medusaUrl = process.env.MEDUSA_BACKEND_URL
+const medusaUrl =
+  process.env.MEDUSA_BACKEND_URL ||
+  "https://medusa-production-18c5.up.railway.app:9000"
 
 export { medusaUrl }
