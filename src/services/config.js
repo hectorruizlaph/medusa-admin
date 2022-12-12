ee97eeadef17ecd05b341974ab904bc6c6a72655
Hector Ruiz <hectorruizlaph@gmail.com>
2022-12-12 12:45:08 -0700

    hey2

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
 const medusaUrl =
-  __MEDUSA_BACKEND_URL__ || "https://medusa-production-18c5.up.railway.app"
+  __MEDUSA_BACKEND_URL__ || "https://medusa-production-18c5.up.railway.app:9000"
 
 export { medusaUrl }
